7023a54c02b55755035ade09ed04c81652d5fb2f
Oleh Halay <o.galai@temabit.com>
2024-02-26 19:34:14 +0200

    Add bad request for failed result

--- JobRouteBuilderExtension.cs
+++ JobRouteBuilderExtension.cs
@@ -4,16 +4,23 @@
     {
         group.MapGet("/{id}", (string id, Store store, HttpResponse httpResponse) =>
         {
-          if (!store.Jobs.TryGetValue(id, out var job) || job is null) 
-              return Results.NotFound();
+            if (!store.Jobs.TryGetValue(id, out var job) || job is null)
+                return Results.NotFound();
 
-          if (job.Status == JobStatus.Completed && !string.IsNullOrEmpty(job.Location)) 
-              return Results.Redirect(job.Location);
 
-          if (job.Status == JobStatus.InProgress)
-              httpResponse.Headers.RetryAfter = TimeSpan.FromSeconds(5).ToString();
+            var okResult = () =>
+            {
+                httpResponse.Headers.RetryAfter = TimeSpan.FromSeconds(5).ToString();
+                return Results.Ok(job);
+            };
 
-          return Results.Ok(job);
+            return job.Status switch
+            {
+                JobStatus.Completed => Results.Redirect(job.Location),
+                JobStatus.InProgress => okResult(),
+                JobStatus.Failed => Results.BadRequest(job),
+                _ => throw new NotImplementedException(),
+            };
         });
 
         return group;
